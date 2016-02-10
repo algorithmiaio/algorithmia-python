8878eb1507fca36624fbc19cb8955bfc54c0b09c
James Athappilly <jamesatha@gmail.com>
2016-02-10 10:42:14 -0800

    tabs to spaces

--- Test/datadirectorytest.py
+++ Test/datadirectorytest.py
@@ -9,12 +9,12 @@ from Algorithmia.datadirectory import DataDirectory
 import os
 
 class DataDirectoryTest(unittest.TestCase):
-	def setUp(self):
-		self.client = client(os.environ['ALGORITHMIA_API_KEY'])
+    def setUp(self):
+        self.client = client(os.environ['ALGORITHMIA_API_KEY'])
 
-	def test_directory_does_not_exit(self):
-		dd = DataDirectory(self.client, "data://.my/this_should_never_be_created")
-		self.assertFalse(dd.exists())
+    def test_directory_does_not_exit(self):
+        dd = DataDirectory(self.client, "data://.my/this_should_never_be_created")
+        self.assertFalse(dd.exists())
 
 if __name__ == '__main__':
     unittest.main()
