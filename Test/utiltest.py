7205ca43cfb7576b217f34fefebb288545609926
James Athappilly <jamesatha@gmail.com>
2016-02-11 00:11:03 -0800

    Add some tests for the util getParentAndBase method

---
+++ Test/utiltest.py
@@ -0,0 +1,21 @@
+import sys
+sys.path.append("../")
+
+import unittest
+
+import Algorithmia
+from Algorithmia.util import getParentAndBase
+
+class UtilTest(unittest.TestCase):
+    def test_getParentAndBase(self):
+        self.assertEqual(('a/b', 'c'), getParentAndBase('a/b/c'))
+        self.assertEqual(('a/b', 'c'), getParentAndBase('a/b/c///'))
+        self.assertEqual(('//a//b', 'c'), getParentAndBase('//a//b////c///'))
+
+    def test_getParentAndBase_errors(self):
+        self.assertRaises(Exception, getParentAndBase('/'))
+        self.assertRaises(Exception, getParentAndBase(''))
+        self.assertRaises(Exception, getParentAndBase('a/'))
+
+if __name__ == '__main__':
+    unittest.main()
