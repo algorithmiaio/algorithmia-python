9a6ea33dab9f6de5b7cb2ae4ea36aea8a75104d3
Jonathan Word <argoday@argoday.com>
2016-10-25 18:46:29 +0000

    Add unicode tests

--- Test/algo_test.py
+++ Test/algo_test.py
@@ -15,5 +15,31 @@ class AlgoTest(unittest.TestCase):
         self.assertEquals('binary', result.metadata.content_type)
         self.assertEquals(bytearray('foo','utf-8'), result.result)
 
+    def test_text_unicode(self):
+        telephone = u"\u260E"
+
+        #Unicode input to pipe()
+        result1 = self.client.algo('util/Echo').pipe(telephone)
+        self.assertEquals('text', result1.metadata.content_type)
+        self.assertEquals(telephone, result1.result)
+
+        #Unicode return in .result
+        result2 = self.client.algo('util/Echo').pipe(result1.result)
+        self.assertEquals('text', result2.metadata.content_type)
+        self.assertEquals(telephone, result2.result)
+
+    def test_json_unicode(self):
+        telephone = [u"\u260E"]
+
+        #Unicode input to pipe()
+        result1 = self.client.algo('util/Echo').pipe(telephone)
+        self.assertEquals('json', result1.metadata.content_type)
+        self.assertEquals(telephone, result1.result)
+
+        #Unicode return in .result
+        result2 = self.client.algo('util/Echo').pipe(result1.result)
+        self.assertEquals('json', result2.metadata.content_type)
+        self.assertEquals(telephone, result2.result)
+
 if __name__ == '__main__':
     unittest.main()
