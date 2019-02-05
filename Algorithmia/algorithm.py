12874ebaf10a983530add578da142b2612f24085
Besir Kurtulmus <besir@algorithmia.com>
2019-02-05 02:47:25 -0800

    Support algorithn .create() .update() .publish() and .getInfo() methods

--- Algorithmia/algorithm.py
+++ Algorithmia/algorithm.py
@@ -6,6 +6,9 @@ from Algorithmia.async_response import AsyncResponse
 from Algorithmia.algo_response import AlgoResponse
 from Algorithmia.errors import ApiError, ApiInternalError
 from enum import Enum
+from algorithmia_api_client.rest import ApiException
+from algorithmia_api_client import CreateRequest, UpdateRequest, VersionRequest, Details, Settings, SettingsMandatory, SettingsPublish, \
+    CreateRequestVersionInfo, VersionInfo
 
 OutputType = Enum('OutputType','default raw void')
 
@@ -17,6 +20,10 @@ class Algorithm(object):
         if m is not None:
             self.client = client
             self.path = m.group(1)
+            self.username = self.path.split("/")[0]
+            self.algoname = self.path.split("/")[1]
+            if len(self.path.split("/")) > 2:
+                self.version = self.path.split("/")[2]
             self.url = '/v1/algo/' + self.path
             self.query_parameters = {}
             self.output_type = OutputType.default
@@ -28,6 +35,61 @@ class Algorithm(object):
         self.output_type = output
         self.query_parameters.update(query_parameters)
         return self
+
+    # Create a new algorithm
+    def create(self, details, settings, version_info):
+        detailsObj = Details(**details)
+        settingsObj = SettingsMandatory(**settings)
+        createRequestVersionInfoObj = CreateRequestVersionInfo(**version_info)
+        create_parameters = {"name": self.algoname, "details": detailsObj, "settings": settingsObj, "version_info": createRequestVersionInfoObj}
+        create_request = CreateRequest(**create_parameters)
+        try:
+            # Create Algorithm
+            api_response = self.client.manageApi.create_algorithm(self.username, create_request)
+            print(api_response)
+            return api_response
+        except ApiException as e:
+            print("Exception when calling DefaultApi->create_algorithm: %s\n" % e)
+
+    # Update the settings in an algorithm
+    def update(self, details, settings, version_info):
+        detailsObj = Details(**details)
+        settingsObj = Settings(**settings)
+        createRequestVersionInfoObj = CreateRequestVersionInfo(**version_info)
+        update_parameters = {"details": detailsObj, "settings": settingsObj, "version_info": createRequestVersionInfoObj}
+        update_request = UpdateRequest(**update_parameters)
+        try:
+            # Update Algorithm
+            api_response = self.client.manageApi.update_algorithm(self.username, self.algoname, update_request)
+            print(api_response)
+            return api_response
+        except ApiException as e:
+            print("Exception when calling DefaultApi->update_algorithm: %s\n" % e)
+
+    # Publish an algorithm
+    def publish(self, details, settings, version_info):
+        detailsObj = Details(**details)
+        settingsObj = SettingsPublish(**settings)
+        versionRequestObj = VersionInfo(**version_info)
+        publish_parameters = {"details": detailsObj, "settings": settingsObj, "version_info": versionRequestObj}
+        version_request = VersionRequest(**publish_parameters) # VersionRequest | Publish Version Request
+        try:
+            # Publish Algorithm
+            api_response = self.client.manageApi.publish_algorithm(self.username, self.algoname, version_request)
+            print(api_response)
+            return api_response
+        except ApiException as e:
+            print("Exception when calling DefaultApi->publish_algorithm: %s\n" % e)
+
+    # Get info on an algorithm
+    def getInfo(self):
+        try:
+            # Get Algorithm
+            api_response = self.client.manageApi.get_algorithm(self.username, self.algoname)
+            pprint(api_response)
+            return api_response
+        except ApiException as e:
+            print("Exception when calling DefaultApi->get_algorithm: %s\n" % e)
 
     # Pipe an input into this algorithm
     def pipe(self, input1):
