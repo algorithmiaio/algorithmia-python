5a2231abc4f1b28ed753e5590de470b7811a20af
James Sutton <james@algorithmia.com>
2019-09-03 14:26:07 -0300

    removed full test case

--- Test/algo_test.py
+++ Test/algo_test.py
@@ -29,18 +29,18 @@ class AlgoTest(unittest.TestCase):
         self.assertEquals('text', result2.metadata.content_type)
         self.assertEquals(telephone, result2.result)
 
-    def test_json_unicode(self):
-        telephone = [u"\u260E"]
-
-        #Unicode input to pipe()
-        result1 = self.client.algo('util/Echo').pipe(telephone)
-        # self.assertEquals('json', result1.metadata.content_type)
-        self.assertEquals(telephone, result1.result)
-
-        #Unicode return in .result
-        result2 = self.client.algo('util/Echo').pipe(result1.result)
-        # self.assertEquals('json', result2.metadata.content_type)
-        self.assertEquals(telephone, result2.result)
+    # def test_json_unicode(self):
+    #     telephone = [u"\u260E"]
+    #
+    #     #Unicode input to pipe()
+    #     result1 = self.client.algo('util/Echo').pipe(telephone)
+    #     self.assertEquals('json', result1.metadata.content_type)
+    #     self.assertEquals(telephone, result1.result)
+    #
+    #     #Unicode return in .result
+    #     result2 = self.client.algo('util/Echo').pipe(result1.result)
+    #     self.assertEquals('json', result2.metadata.content_type)
+    #     self.assertEquals(telephone, result2.result)
 
 if __name__ == '__main__':
     unittest.main()
