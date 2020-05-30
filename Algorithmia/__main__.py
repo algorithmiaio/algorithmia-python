f989ebc3555c9b227f8e6460711b0326bea46ac5
BenT00le <btoole@algorithmia.io>
2020-05-29 17:22:50 -0700

    added data commands tested ls,mkdir,rmdir. cp, and auth not currently working

--- Algorithmia/__main__.py
+++ Algorithmia/__main__.py
@@ -3,7 +3,7 @@ import os
 import Algorithmia
 
 #CLI app to allow a user to run algorithms and manage data collections
-commands = ['run', 'auth', 'ls', 'mkdir', ]
+
 usage = """CLI for interaction with Algorithmia
 Usage:
 algo [<cmd>] [options] [<args>...]
@@ -42,7 +42,8 @@ def main():
 		#create a client expecting an apikey to be found in the ALGORITHMIA_API_KEY enviroment variable
 		client = Algorithmia.client()
 		cmd = args[0]
-		#match the command
+
+# algo auth
 		if cmd == 'auth':
 			#auth
 			print("Configuring authentication for 'default' profile")
@@ -52,14 +53,20 @@ def main():
 			#os.environ['ALGORITHMIA_API_KEY'] = APIkey
 			setenv = "export ALGORITHMIA_API_KEY=" + "'" + APIkey + "'"
 			print(setenv)
+
+			# this does not update the enviroment variable properly
 			print(os.system(setenv))
 			os.system("printenv ALGORITHMIA_API_KEY")
 
-		#run the spesified algo
+# algo run <algo> <args..>    run the the spesified algo
 		elif cmd == 'run':
 			#client = Algorithmia.client(os.environ['ALGORITHMIA_API_KEY'])
 			algo_name = args[1]
-			algo_input = args[2]
+
+			if(len(args) > 3):
+				algo_input = args[2:]
+			else:
+				algo_input = args[2]
 
 			algo = client.algo(algo_name);
 			try:
@@ -69,12 +76,66 @@ def main():
 		else:
 			print("in data command")
 			#data command
+			if(len(args) == 2):
+				path = args[1]
+			else:
+				path = None
+
+# algo mkdir <path>
 			if(cmd == "mkdir"):
 				#make a dir in data collection
-				print("mk "+args[2])
+				print("mk "+path)
+				newDir = client.dir(path)
+				
+				if newDir.exists() is False:
+					newDir.create()
+			
+# algo rmdir <path>
+			elif(cmd == "rmdir"):
+				#remove a dir in data collection
+				print("rm "+path)
+				Dir = client.dir(path)
+				
+				if Dir.exists():
+					Dir.delete()
+
+# algo ls <path>			
 			elif(cmd == "ls"):
 				#list dir
-				print("list "+args[2])
+				if(path is None):
+					path = "/.my"
+				print("list "+path)
+				listingDir = client.dir(path)
+				for f in listingDir.list():
+					print(f.getName())
+
+# algo cat <file>
+			elif(cmd == "cat"):
+				file = client.file(path)
+			
+				if(file.exists()):
+					print(file.getString())
+				else:
+					print("file does not exist "+path)
+
+# algo cp <src> <dest>
+			elif(cmd == "cp"):
+
+				if(len(args) < 3):
+					print("expected algo cp <src> <dest>")
+				else:
+					src = args[1]
+					dest = args[2]
+
+					file = client.file(src)
+					destLocation = client.file(dest)
+					
+
+					if(file.exists()):
+						destLocation.putFile(file.getFile())
+
+					else:
+						print("file does not exist")
 
 
 
