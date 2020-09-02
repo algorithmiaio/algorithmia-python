601e3676ee780e63d958e42989c5d06bc2045f87
robert-close <rclose@algorithmia.io>
2020-09-02 15:17:31 -0400

    INSIGHTS-12 Added todo statements

--- Algorithmia/insights.py
+++ Algorithmia/insights.py
@@ -4,10 +4,12 @@ import requests
 class Insights(object):
 
     def __init__(self):
+        # todo how does state work in Python?
         self.insightsCollection = None
 
     def collectInsights(self, insights):
         self.insightsCollection.append(insights)
 
     def reportInsights(self):
+        # todo How will these metrics leave the python algorithm and get to the algorithm-queue-reader? Request post here as place holder
         requests.post("","",self.insightsCollection)
