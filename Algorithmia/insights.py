6b8419029bf2fe60f33ce5f97318e0812adbbb4e
robert-close <rclose@algorithmia.io>
2020-09-03 16:25:57 -0400

    INSIGHTS-12 Added a todo. Tests fail for unknown reasons at this time

--- Algorithmia/insights.py
+++ Algorithmia/insights.py
@@ -4,4 +4,5 @@ import requests
 class Insights(object):
 
     def __init__(self, insights):
+        # todo: pull this from a config.py file?
         requests.post("https://localhost:9000/insights", insights)
