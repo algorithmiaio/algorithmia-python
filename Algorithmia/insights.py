0d9c0056d48de1cb0c3e45008588a4342be4ee63
robert-close <rclose@algorithmia.io>
2020-09-09 09:38:01 -0400

    INSIGHTS-12 Added system property for queue reader url

--- Algorithmia/insights.py
+++ Algorithmia/insights.py
@@ -1,13 +1,11 @@
 import requests
 import json
-
+import os
 
 class Insights:
     # Example of correct insights:
-    # {
-    #   {"aKey":"aValue"},
-    #   {"aKey2":"aValue2"}
-    # }
+    # {"aKey":"aValue","aKey2":"aValue2"}
+    # TODO add this env variable to the deploy config in stage tools?
     def __init__(self, insights):
-        # TODO should we get the URL from a config?
-        requests.post("https://localhost:9000/insights", data=json.dumps(insights).encode('utf-8'))
+        AQR_URL = os.getenv('ALGORITHM_QUEUE_READER_URL') or "https://localhost:9000"
+        requests.post(AQR_URL+"/insights", data=json.dumps(insights).encode('utf-8'))
