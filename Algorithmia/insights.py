71313fdc8e0c9bb19282e4105a47bdc026dcebcb
robert-close <rclose@algorithmia.io>
2020-09-10 11:03:14 -0400

    INSIGHTS-12 Fixed URL to not be https

--- Algorithmia/insights.py
+++ Algorithmia/insights.py
@@ -7,5 +7,8 @@ class Insights:
     # {"aKey":"aValue","aKey2":"aValue2"}
     # TODO add this env variable to the deploy config in stage tools?
     def __init__(self, insights):
-        AQR_URL = os.getenv('ALGORITHM_QUEUE_READER_URL') or "https://localhost:9000"
-        requests.post(AQR_URL+"/insights", data=json.dumps(insights).encode('utf-8'))
+        headers = {}
+        headers['Content-Type'] = 'application/json'
+        AQR_URL = os.getenv('ALGORITHM_QUEUE_READER_URL') or "http://localhost:9000"
+
+        requests.post(AQR_URL+"/insights", data=json.dumps(insights).encode('utf-8'), headers=headers)
