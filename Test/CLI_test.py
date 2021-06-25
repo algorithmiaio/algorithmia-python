abf7f8d42b0c2bd3828d7571287053de25b8b1e9
john.bragg <John.bragg@ruralsourcing.com>
2021-06-25 08:19:50 -0600

    CICD fix

--- Test/CLI_test.py
+++ Test/CLI_test.py
@@ -185,7 +185,8 @@ class CLITest(unittest.TestCase):
 		filename = "./temptest"
 		envid = "400883fb-af98-44c7-8431-0439a56cea40"
 		response = CLI().get_template(envid,filename,self.client)
-		self.assertTrue(response.status_code == 200)
+		print(response)
+		self.assertTrue(response['status_code'] == 200)
 		try:
 			shutil.rmtree(filename)
 		except OSError as e:
