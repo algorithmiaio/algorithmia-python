d7f3db7c0b4d45d33bd8d19a42ccfe5842c77151
john.bragg <John.bragg@ruralsourcing.com>
2021-06-28 16:46:46 -0600

    change assertion in unit test

--- Test/client_test.py
+++ Test/client_test.py
@@ -60,9 +60,10 @@ class client_test(unittest.TestCase):
     def test_get_supported_languages(self):
         client = Algorithmia.client(api_key=os.environ.get('ALGORITHMIA_API_KEY'))
         response = client.get_supported_languages()
+        language_found = any('anaconda3' in languages['name'] for languages in response)
         if("error" in response):
             print(response)
-        self.assertTrue(response is not None and "name" in response[0])
+        self.assertTrue(response is not None and language_found)
 
     def test_invite_to_org(self):
         response = self.c.invite_to_org("a_myOrg38","a_Mrtest4")
