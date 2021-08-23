3b71119aff5454bbc9d505d2b9fa6b2f8c331472
zeryx <1892175+zeryx@users.noreply.github.com>
2021-08-23 14:09:27 -0300

    verified functionality with api-client version 1.5.5

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
         'enum-compat',
         'toml',
         'argparse',
-        'algorithmia-api-client==1.6.0',
+        'algorithmia-api-client==1.5.5',
         'algorithmia-adk>=1.0.2,<1.1'
     ],
     include_package_data=True,
