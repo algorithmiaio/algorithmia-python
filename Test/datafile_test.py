2379c5d1c7bd76bd193ea696d42d2236c9068d01
zeryx <1892175+zeryx@users.noreply.github.com>
2021-08-26 12:36:51 -0300

    updated datafile test to close the file

--- Test/datafile_test.py
+++ Test/datafile_test.py
@@ -147,7 +147,8 @@ class AdvancedDataFileTest(unittest.TestCase):
             payload = {"hello": "world"}
         response = df.putJson(payload)
         self.assertEqual(response.path,file)
-        result = json.loads(df.read())
+        with df as f:
+            result = json.loads(f.read())
         self.assertDictEqual(result, payload)
         self.assertEqual(str(result), str(payload))
 
