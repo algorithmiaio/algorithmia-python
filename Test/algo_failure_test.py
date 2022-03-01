f911e556fdf631ab82599f35275e4ce848cbe654
James Sutton <1892175+zeryx@users.noreply.github.com>
2022-03-01 12:39:21 -0400

    improvement for systemic errors (#122)
    * added a better message when errors are not caused by Algorithms
    
    * make failure test compliant with other testcase components

--- Test/algo_failure_test.py
+++ Test/algo_failure_test.py
@@ -14,22 +14,14 @@ if sys.version_info[0] >= 3:
     from Test.api import app
 
 
-    def start_webserver():
-        uvicorn.run(app, host="127.0.0.1", port=8080, log_level="debug")
-
-
     class AlgoTest(unittest.TestCase):
         error_500 = Response()
         error_500.status_code = 500
+        error_message = "Non-Algorithm related Failure: " + str(error_500)
 
-        def setUp(self):
-            self.client = Algorithmia.client(api_address="http://localhost:8080")
-            self.uvi_p = Process(target=start_webserver)
-            self.uvi_p.start()
-            time.sleep(1)
-
-        def tearDown(self):
-            self.uvi_p.terminate()
+        @classmethod
+        def setUpClass(cls):
+            cls.client = Algorithmia.client(api_address="http://localhost:8080", api_key="simabcd123")
 
         def test_throw_500_error_HTTP_response_on_algo_request(self):
             try:
@@ -37,4 +29,4 @@ if sys.version_info[0] >= 3:
             except Exception as e:
                 result = e
                 pass
-            self.assertEqual(str(self.error_500), str(result))
+            self.assertEqual(str(self.error_message), str(result))
